36810e183a5ad17c088bad72bfb27e595090f1a4
Rahul25052002 <rnikale55@gmail.com>
2024-12-15 19:38:14 +0530

    GFG_POTD

---
+++ java DSA/GfG/peakElement.java
@@ -0,0 +1,99 @@
+/*
+ *Peak element
+Difficulty: BasicAccuracy: 38.86%Submissions: 503K+Points: 1
+Given an array arr[] where no two adjacent elements are same, find the index of a peak element. An element is considered to be a peak if it is greater than its adjacent elements (if they exist). If there are multiple peak elements, return index of any one of them. The output will be "true" if the index returned by your function is correct; otherwise, it will be "false".
+
+Note: Consider the element before the first element and the element after the last element to be negative infinity.
+
+Examples :
+
+Input: arr = [1, 2, 4, 5, 7, 8, 3]
+Output: true
+Explanation: arr[5] = 8 is a peak element because arr[4] < arr[5] > arr[6].
+Input: arr = [10, 20, 15, 2, 23, 90, 80]
+Output: true
+Explanation: arr[1] = 20 and arr[5] = 90 are peak elements because arr[0] < arr[1] > arr[2] and arr[4] < arr[5] > arr[6]. 
+Input: arr = [1, 2, 3]
+Output: true
+Explanation: arr[2] is a peak element because arr[1] < arr[2] and arr[2] is the last element, so it has negative infinity to its right.
+Constraints:
+1 ≤ arr.size() ≤ 106
+-231 ≤ arr[i] ≤ 231 - 1
+ */
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            int idx = obj.peakElement(a);
+            int n = a.length;
+            if (idx < 0 && idx >= n)
+                System.out.println("false");
+            else {
+                if (n == 1 && idx == 0)
+                    f = 1;
+                else if (idx == 0 && a[0] > a[1])
+                    f = 1;
+                else if (idx == n - 1 && a[n - 1] > a[n - 2])
+                    f = 1;
+                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
+                         a[idx] > a[idx - 1])
+                    f = 1;
+                else
+                    f = 0;
+                if (f == 1) {
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+
+        if(arr.length==1){
+            return 0;
+        }
+
+        int store=0;
+        if(arr[0]>arr[1]){
+            return  store;
+        }
+        for(int i=1;i<arr.length-1;i++){
+            store=i;
+            if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+                return store;
+            }
+        }
+        if(arr[arr.length-1]>arr[arr.length-2]){
+            return arr.length-1;
+        }
+        return -1;
+    }
+}
+
---
+++ java DSA/timepass/cdac.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	 //char boolean[][6]={"TRUE","FALSE"};
+	 //printf("%s",boolean[(unsigned int)-1 == ~0]);
+
+	 int n=0;
+	 if(n++){
+		 printf("C-DAC");
+	 }else if(n--){
+		 printf("ACTS");
+	 }
+}
